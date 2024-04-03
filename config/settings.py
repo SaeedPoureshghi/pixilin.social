d48c4ba3ffd43dc6ff58482d41822b6c402b1c33
SaeedPoureshghi <saeed.po@gmail.com>
2024-04-03 12:24:52 +0300

    Update Debug mode

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ke)ei=qzr*dd_*_ws1fh4iqdd(f*6c$+#_m)z99s*wqpiwtarh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['199.188.203.210','api.pixilin.social']
 
